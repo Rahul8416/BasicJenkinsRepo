f7cace6dffb807cb93b0656a67f2c105ab76d03b
Rahul <kumarrahul8416@gmail.com>
2024-08-28 01:35:02 +0530

    this is last line commit

--- src/basicPackag/MainClass.java
+++ src/basicPackag/MainClass.java
@@ -7,6 +7,7 @@ public class MainClass {
 		System.out.println("This is Java Profram for jenskins");
 		System.out.println("This is for testing purpuse");
 		System.out.println("This is 3rd lie of this main methods");
+		System.out.println("This will be last message for check the status in github");
 
 	}
 
