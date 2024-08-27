33c5cc9e81f294ab07cc7d52e5848c20a8abe211
Rahul <kumarrahul8416@gmail.com>
2024-08-28 01:27:26 +0530

    first commit

---
+++ src/basicPackag/MainClass.java
@@ -0,0 +1,12 @@
+package basicPackag;
+
+public class MainClass {
+
+	public static void main(String[] args)
+	{
+		System.out.println("This is Java Profram for jenskins");
+		System.out.println("This is for testing purpuse");
+
+	}
+
+}
